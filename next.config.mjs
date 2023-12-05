8b952a24c771c80d8643d3f0470a695525cb8556
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-12-05 18:29:41 +0900

    🤡 chore:  이미지 도메인, proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    emotion: true
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'openweathermap.org' }]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`
+      }
+    ];
+  }
+};
 
 export default nextConfig;
